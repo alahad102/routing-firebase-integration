45908ed4090302173c329bf6c4729893aba9c857
alahad102 <alahad102@gmail.com>
2022-05-15 14:21:58 +0600

    signout

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import useFirebase from '../../hooks/useFirebase';
 
 const Login = () => {
+    const {signInWithGoogle} = useFirebase();
+
     return (
         <div>
             <h2>please login</h2>
+            
+            <div style={{margin: '20px'}}>
+                <button onClick={signInWithGoogle}>Google Sign in</button>
+            </div>
+            <form>
+                <input type="email" name="" id="" placeholder='Your email' />
+                <br />
+                <input type="password" name="" id="" placeholder='your password' />
+                <br />
+                <input type="submit" value="Login" />
+
+
+            </form>
             
         </div>
     );
---
+++ src/Components/Register/Register.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Register = () => {
+    return (
+        <div>
+            <h3>please register now</h3>
+            <form>
+                <input type="text" name="" id="" placeholder='your name' />
+                <br />
+                <input type="email" name="" id="" placeholder='Your email' />
+                <br />
+                <input type="password" name="" id="" placeholder='your password' />
+                <br />
+                <input type="submit" value="Register" />
+
+ 
+            </form>
+        </div>
+    );
+};
+
+export default Register;
